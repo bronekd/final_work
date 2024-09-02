8f4eee1cd3a298acc193480fb493a7298e0c084f
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-09-03 00:01:45 +0200

    přidání vzorových dat

--- project-tempalte/my_web/core/urls.py
+++ project-tempalte/my_web/core/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import *
 from .views import about_page_view, terms_and_conditions
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", home_page_view, name="home"),
     path("about", about_page_view, name="about"),
     path('terms-and-conditions', views.terms_and_conditions, name='terms_and_conditions'),
-]
+    path('download-image/', views.download_image, name='download_image'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project-tempalte/my_web/core/views.py
+++ project-tempalte/my_web/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import FileResponse
+
 
 def home_page_view(request):
     return render(request, "pages/home.html")
@@ -13,13 +13,17 @@ def terms_and_conditions(request):
     return render(request, 'pages/terms_and_conditions.html')
 
 
-
-def download_model(request):
-    file_path = 'path/to/your/model.h5'
-    response = FileResponse(open(file_path, 'rb'))
-    return response
+from django.http import HttpResponse
+from django.conf import settings
+import os
 
 def download_image(request):
-    image_path = 'path/to/your/image.png'
-    response = FileResponse(open(image_path, 'rb'))
-    return response
+    image_path = os.path.join(settings.MEDIA_ROOT, 'vzor/Kolecko_01.png')
+    if os.path.exists(image_path):
+        with open(image_path, 'rb') as f:
+            data = f.read()
+        response = HttpResponse(data, content_type='image/png')
+        response['Content-Disposition'] = 'attachment; filename="Kolecko_01.png"'
+        return response
+    else:
+        return HttpResponse('Soubor nenalezen.', status=404)
