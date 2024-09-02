b2efc242f229781ae45686d1f0569ab7467cc58d
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-09-02 23:09:17 +0200

    Update views.py

--- project-tempalte/my_web/core/views.py
+++ project-tempalte/my_web/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import FileResponse
 
 def home_page_view(request):
     return render(request, "pages/home.html")
@@ -11,3 +11,15 @@ def about_page_view(request):
 
 def terms_and_conditions(request):
     return render(request, 'pages/terms_and_conditions.html')
+
+
+
+def download_model(request):
+    file_path = 'path/to/your/model.h5'
+    response = FileResponse(open(file_path, 'rb'))
+    return response
+
+def download_image(request):
+    image_path = 'path/to/your/image.png'
+    response = FileResponse(open(image_path, 'rb'))
+    return response
